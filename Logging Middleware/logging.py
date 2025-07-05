8a5352a282aea2a3582e5d52e812c61c2b8e4779
VISWA68 <you@example.com>
2025-07-05 11:27:32 +0530

    created a resuable log function that takes the required params and makes a call to the server and also logs tge response

---
+++ Logging Middleware/logging.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+LOGGING_URL = os.getenv("LOGGING_URL")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+
+def log(stack, level, package, message):
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "stack": stack,
+        "level": level,
+        "package": package,
+        "message": message
+    }
+
+    try:
+        response = requests.post(LOGGING_URL, headers=headers, json=payload)
+        if response.status_code == 200:
+            print(response["logID"])
+            print(response["message"])
+    except requests.exceptions.RequestException as e:
+        print(e)
